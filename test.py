ac694ac2f8d06c7db52fbe3badb771441ceb40cb
Sanika-selvadasan <sanikaselvadasank@gmail.com>
2024-01-06 15:19:54 +0530

    commit3

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from happytransformer import TTSettings
 
 happy_tt = HappyTextToText("T5", "prithivida/grammar_error_correcter_v1")
 
-text = "gec: " + "We no open, sorrry"
+text = "gec: " + "nature when you smile over your shoulder. for a minute i forget that i'm older. I wanna dance with you right now.and you ask me to stay over. i so in love with you. i hope you knw."
 settings = TTSettings(do_sample=True, top_k=10, temperature=0.5,  min_length=1, max_length=100)
 result = happy_tt.generate_text(text, args=settings)
 
